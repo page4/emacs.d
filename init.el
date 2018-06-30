37255adc4bbde7c73f750c632f86555de812df2d
page4 <page4a@gmail.com>
2018-06-30 13:37:16 +0800

    add pyim-wbdict

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (projectile neotree jdee htmlize org-journal solarized-theme org-link-minor-mode pyim tuareg evil evil-leader))))
+    (projectile neotree jdee htmlize org-journal solarized-theme org-link-minor-mode pyim pyim-wbdict tuareg evil evil-leader))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
